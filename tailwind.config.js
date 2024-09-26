2afd0644b0dc2d866bd55bab4b3dd4cd0b6c6f91
Gildo-Henriques <gildohenriques520@gmail.com>
2024-09-26 16:57:40 +0100

    Actualizando

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",  // Inclui arquivos dentro da pasta src
+    "./public/**/*.{html,js}", // Adiciona a pasta public
+    "./components/**/*.{html,js}", // Adiciona a pasta components (ou qualquer outra que desejar)
+    "./**/*.{html,js}" // Inclui todos os arquivos HTML e JS do projeto inteiro
+  ],
   theme: {
     extend: {
       colors: {
         escuro: {
-          "100" : "#1a1a1a"
+          "100": "#1a1a1a",
         },
         sangue: {
-          "100": "#e40326"
-        }
-      }
+          "100": "#e40326",
+        },
+      },
     },
   },
   plugins: [],
